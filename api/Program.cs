c807bf4650c3c7460fb2bf3c91d46a895455242b
hrshuv0 <h.r.shuvo9@gmail.com>
2024-03-10 01:15:49 +0600

    add db config

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,13 @@
+using Framework.Core.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
